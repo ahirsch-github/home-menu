c72ebdb1f5c1e73f4c1a8556b338929b85cfbf6b
ahirsch-github <anja_hirsch@t-online.de>
2022-09-20 17:07:15 +0200

    feat(0.0): initial project setup with spring initializer

---
+++ src/test/java/com/food/homemenu/HomeMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.homemenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeMenuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
